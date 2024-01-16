545d2220935695a2682b88b0ea50b6dc6ac6769a
Anna Aksenov <annaaksenov8@gmail.com>
2024-01-15 21:06:40 -0800

    Initial commit

---
+++ server/lib/index.ts
@@ -0,0 +1,4 @@
+export * from './client-error.js';
+export * from './error-middleware.js';
+export * from './authorization-middleware.js';
+export * from './default-middleware.js';
---
+++ server/server.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove when used */
+import 'dotenv/config';
+import express from 'express';
+import pg from 'pg';
+import {
+  ClientError,
+  defaultMiddleware,
+  errorMiddleware,
+} from './lib/index.js';
+
+const connectionString =
+  process.env.DATABASE_URL ||
+  `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
+const db = new pg.Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+
+// Create paths for static directories
+const reactStaticDir = new URL('../client/dist', import.meta.url).pathname;
+const uploadsStaticDir = new URL('public', import.meta.url).pathname;
+
+app.use(express.static(reactStaticDir));
+// Static directory for file uploads server/public/
+app.use(express.static(uploadsStaticDir));
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+/*
+ * Middleware that handles paths that aren't handled by static middleware
+ * or API route handlers.
+ * This must be the _last_ non-error middleware installed, after all the
+ * get/post/put/etc. route handlers and just before errorMiddleware.
+ */
+app.use(defaultMiddleware(reactStaticDir));
+
+app.use(errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+  process.stdout.write(`\n\napp listening on port ${process.env.PORT}\n\n`);
+});
