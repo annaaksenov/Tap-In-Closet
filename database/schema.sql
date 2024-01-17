cbb975d8b3f48cc6d294da510526dbe20d9d198d
Anna Aksenov <annaaksenov8@gmail.com>
2024-01-17 03:39:18 +0000

    Initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,43 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY,
+  "username" varchar(255),
+  "hashedPassword" varchar(255),
+  "email" varchar(255),
+  "createdAt" timestamp(6),
+  "updatedAt" timestamp(6)
+);
+
+CREATE TABLE "closet" (
+  "itemId" serial PRIMARY KEY,
+  "userId" integer,
+  "image" text,
+  "category" varchar(100),
+  "description" text,
+  "hashtag" text,
+  "createdAt" timestamp
+);
+
+CREATE TABLE "outfits" (
+  "outfitId" serial PRIMARY KEY,
+  "userId" integer,
+  "name" varchar(255),
+  "createdAt" timestamp(6),
+  "updatedAt" timestamp(6)
+);
+
+CREATE TABLE "favorites" (
+  "favoriteId" serial PRIMARY KEY,
+  "userId" integer,
+  "outfitId" integer,
+  "createdAt" timestamp(6)
+);
+
+ALTER TABLE "closet" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "outfits" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "favorites" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
